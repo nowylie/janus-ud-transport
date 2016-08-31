a20a6b620ba2195c7bd3061833e272bfc00c03d1
Nicholas Wylie <nowylie@gmail.com>
2016-08-31 10:35:39 +1000

    update log output

--- unix-dgram.c
+++ unix-dgram.c
@@ -75,7 +75,7 @@ static janus_transport ud_plugin = JANUS_TRANSPORT_INIT (
 
 /* Transport creator */
 janus_transport *create(void) {
-	JANUS_LOG(LOG_VERB, "%s created!\n", JANUS_UD_NAME);
+	JANUS_LOG(LOG_VERB, "[%s] created!\n", JANUS_UD_PACKAGE);
 	return &ud_plugin;
 }
 
@@ -167,7 +167,7 @@ void *recv_thread(void *data) {
 	fds[0].events = POLLIN;
 	
 	
-	JANUS_LOG(LOG_INFO, "%s unix thread started\n", (admin ? "Admin" : "Janus"));
+	JANUS_LOG(LOG_INFO, "[%s] %s unix thread started\n", JANUS_UD_PACKAGE, (admin ? "Admin" : "Janus"));
 	while (!stop) {
 		pthread_mutex_lock(&thread_lock);
 		stop = thread_stop;
@@ -177,7 +177,7 @@ void *recv_thread(void *data) {
 		// poll the socket
 		int left = poll(fds, 1, POLL_TIMEOUT);
 		if (left == -1) { // an error occured
-			JANUS_LOG(LOG_ERR, "poll: %s\n", strerror(errno));
+			JANUS_LOG(LOG_ERR, "[%s] poll: %s\n", JANUS_UD_PACKAGE, strerror(errno));
 			break;
 		} else if (left == 0) { // timed out
 			continue;
@@ -190,19 +190,19 @@ void *recv_thread(void *data) {
 		addrlen = sizeof(struct sockaddr_storage);
 		ssize_t n = recvfrom(fds[0].fd, b.data, BMAX, 0, (struct sockaddr *)&addr, &addrlen);
 		if (n == -1) { // an error occured
-			JANUS_LOG(LOG_ERR, "recvfrom: %s\n", strerror(errno));
+			JANUS_LOG(LOG_ERR, "[%s] recvfrom: %s\n", JANUS_UD_PACKAGE, strerror(errno));
 			break;
 		}
 		b.data[n] = '\0';
 		b.length = n;
 		
-		JANUS_LOG(LOG_HUGE, "received: %s\n", b.data);
+		JANUS_LOG(LOG_HUGE, "[%s] received: %s\n", JANUS_UD_PACKAGE, b.data);
 		
 		// get a pointer to the cached version of this address
 		struct sockaddr_un *uaddr, *caddr;
 		uaddr = (struct sockaddr_un *)&addr;
 		if (uaddr->sun_path[0] == '\0') {
-			JANUS_LOG(LOG_WARN, "dropping request from anonymous socket\n");
+			JANUS_LOG(LOG_WARN, "[%s] dropping request from anonymous socket\n", JANUS_UD_PACKAGE);
 			continue;
 		}
 
@@ -213,18 +213,19 @@ void *recv_thread(void *data) {
 			g_hash_table_insert(addr_cache, uaddr->sun_path, caddr);
 		}
 		
-		JANUS_LOG(LOG_HUGE, "remote address: %s\n", uaddr->sun_path);
+		JANUS_LOG(LOG_HUGE, "[%s] remote address: %s\n", JANUS_UD_PACKAGE, uaddr->sun_path);
 		
 		// decode the message
 		json_error_t err;
 		json_t *msg = json_loadb(b.data, b.length, JSON_DISABLE_EOF_CHECK, &err);
 		if (msg == NULL) { // an error occured
-			JANUS_LOG(LOG_ERR, "json_loadb: %s\n", err.text);
+			JANUS_LOG(LOG_ERR, "[%s] json_loadb: %s\n", JANUS_UD_PACKAGE, err.text);
 			continue;
 		}
 
 		// what kind of request is this?
 		const gchar *method = json_string_value(json_object_get(msg, "janus"));
+		JANUS_LOG(LOG_VERB, "[%s] Received '%s' request\n", JANUS_UD_PACKAGE, method);
 		
 		if (!strcasecmp(method, "info")) {
 			json_t *reply = gateway->janus_info(json_string_value(json_object_get(msg, "transaction")));
@@ -252,7 +253,7 @@ void *recv_thread(void *data) {
 	thread_count--;
 	if (!thread_count) pthread_cond_signal(&thread_cv);
 	pthread_mutex_unlock(&thread_lock);
-	JANUS_LOG(LOG_INFO, "%s thread stopped\n", (admin ? "admin" : "janus"));
+	JANUS_LOG(LOG_INFO, "[%s] %s thread stopped\n", JANUS_UD_PACKAGE, (admin ? "admin" : "janus"));
 	
 	return NULL;
 }
@@ -274,18 +275,18 @@ int send_message(struct sockaddr_un *addr, gboolean admin, json_t *msg) {
 	
 	b.length = 0;
 	if (json_dump_callback(msg, (json_dump_callback_t)dump_callback, &b, JSON_COMPACT)) {
-		JANUS_LOG(LOG_ERR, "json_dump_callback: encoding too long for buffer\n");
+		JANUS_LOG(LOG_ERR, "[%s] json_dump_callback: encoding too long for buffer\n", JANUS_UD_PACKAGE);
 		return -1;
 	}
 	b.data[b.length] = '\0';
 
-	JANUS_LOG(LOG_HUGE, "sending: %s\n", b.data);
-	JANUS_LOG(LOG_HUGE, "remote address: %s\n", addr->sun_path);
+	JANUS_LOG(LOG_HUGE, "[%s] sending: %s\n", JANUS_UD_PACKAGE, b.data);
+	JANUS_LOG(LOG_HUGE, "[%s] remote address: %s\n", JANUS_UD_PACKAGE, addr->sun_path);
 
 	socklen_t addrlen = UN_LEN(addr);
 	ssize_t n = sendto(fd, b.data, b.length, 0, (struct sockaddr *)addr, addrlen);
 	if (n == -1) {
-		JANUS_LOG(LOG_ERR, "sendto: %s\n", strerror(errno));
+		JANUS_LOG(LOG_ERR, "[%s] sendto: %s\n", JANUS_UD_PACKAGE, strerror(errno));
 		return -1;
 	}
 	
@@ -304,11 +305,11 @@ int janus_ud_init(janus_transport_callbacks *callbacks, const char *cpath) {
 
 	char filename[255];
 	snprintf(filename, 255, "%s/%s.cfg", cpath, JANUS_UD_PACKAGE);
-	JANUS_LOG(LOG_VERB, "Configuration file: %s\n", filename);
+	JANUS_LOG(LOG_VERB, "[%s] Configuration file: %s\n", JANUS_UD_PACKAGE, filename);
 
 	janus_config *config = janus_config_parse(filename);
 	if (config == NULL) {
-		JANUS_LOG(LOG_FATAL, "Unable to parse configuration file\n");
+		JANUS_LOG(LOG_FATAL, "[%s] Unable to parse configuration file\n", JANUS_UD_PACKAGE);
 		return -1;
 	}
 
@@ -322,7 +323,7 @@ int janus_ud_init(janus_transport_callbacks *callbacks, const char *cpath) {
 		memset(path, '\0', PMAX);
 		strncpy(path, item->value, PMAX);
 		if (path[PMAX - 1] != '\0') {
-			JANUS_LOG(LOG_FATAL, "Admin socket path is too long");
+			JANUS_LOG(LOG_FATAL, "[%s] Admin socket path is too long\n", JANUS_UD_PACKAGE);
 			return -1;
 		}
 		
@@ -332,7 +333,7 @@ int janus_ud_init(janus_transport_callbacks *callbacks, const char *cpath) {
 		// Create Admin socket
 		adminfd = new_unix_socket(path, &err);
 		if (adminfd == -1) {
-			JANUS_LOG(LOG_ERR, "new_unix_socket: %s\n", strerror(err));
+			JANUS_LOG(LOG_ERR, "[%s] new_unix_socket: %s\n", JANUS_UD_PACKAGE, strerror(err));
 		}
 		
 		// Start receiving on socket
@@ -347,7 +348,7 @@ int janus_ud_init(janus_transport_callbacks *callbacks, const char *cpath) {
 		memset(path, '\0', PMAX);
 		strncpy(path, item->value, PMAX);
 		if (path[PMAX - 1] != '\0') {
-			JANUS_LOG(LOG_FATAL, "Janus socket path is too long");
+			JANUS_LOG(LOG_FATAL, "[%s] Janus socket path is too long\n", JANUS_UD_PACKAGE);
 			return -1;
 		}
 		
@@ -357,14 +358,14 @@ int janus_ud_init(janus_transport_callbacks *callbacks, const char *cpath) {
 		// Create Janus socket
 		janusfd = new_unix_socket(path, &err);
 		if (janusfd == -1) {
-			JANUS_LOG(LOG_ERR, "new_unix_socket: %s\n", strerror(err));
+			JANUS_LOG(LOG_ERR, "[%s] new_unix_socket: %s\n", JANUS_UD_PACKAGE, strerror(err));
 		}
 
 		// Start receiving on socket
 		new_thread(recv_thread, NULL);
 	}
 	
-	JANUS_LOG(LOG_INFO, "%s started\n", JANUS_UD_NAME);
+	JANUS_LOG(LOG_INFO, "[%s] started\n", JANUS_UD_PACKAGE);
 	return 0;
 }
 
@@ -374,7 +375,7 @@ void janus_ud_destroy(void) {
 	if (adminfd) close(adminfd);
 	if (janusfd) close(janusfd);
 	g_hash_table_destroy(addr_cache);
-	JANUS_LOG(LOG_INFO, "%s stopped\n", JANUS_UD_NAME);
+	JANUS_LOG(LOG_INFO, "[%s] stopped\n", JANUS_UD_PACKAGE);
 }
 
 int janus_ud_get_api_compatibility(void) {
